deadb90b63f2f074af9c499094e66c86a4539127
verifyMe <ms.groma@gmail.com>
2020-10-20 23:14:45 +0300

    remove unnessesary

--- bankTest/src/main/java/weather/WeatherTest.java
+++ bankTest/src/main/java/weather/WeatherTest.java
@@ -5,17 +5,10 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import weather.data.WeatherTestData;
 import weather.data.WeatherURI;
-import weather.util.MapMatcher;
-
-import java.util.Map;
 
 import org.apache.http.HttpStatus;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.collection.IsMapContaining.hasKey;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 public abstract class WeatherTest {
     protected static RequestSpecification requestSpec = new RequestSpecBuilder()
@@ -32,21 +25,4 @@ public abstract class WeatherTest {
             .build();
 
 
-    protected void assertEqualToExpected(Map<?, ?> tested, Map<?,?> expected){
-        expected.entrySet()
-                .forEach(entry-> {
-                            assertThat(tested, hasKey(entry.getKey()));
-                            assertThat("For the field "+entry.getKey(),
-                                    tested.get(entry.getKey()),
-                                    equalTo(entry.getValue()));
-                        }
-                );
-    }
-
-    protected void assertEqualToExpected(WeatherTestData tested, WeatherTestData expected){
-        assertThat(tested.getLocation(), new MapMatcher(expected.getLocation()));
-        assertEqualToExpected(tested.getRequest(), expected.getRequest());
-        assertEqualToExpected(tested.getLocation(), expected.getLocation());
-        assertEqualToExpected(tested.getCurrent(), expected.getCurrent());
-    }
 }
